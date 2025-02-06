5457ec6826eda4643612460a27d17f676a2716a7
David Medeiros <davidmedeiros94@hotmail.com>
2025-02-06 17:03:51 -0300

    otimizado

--- src/test/java/TestCalculadora.java
+++ src/test/java/TestCalculadora.java
@@ -1,12 +1,14 @@
 // 1- Pacotes do projeto
 
 // 2- Bibliotecas / Imports
-import org.junit.jupiter.api.Test; // JUnit 5
-import com.iterasys.Main; // Classe que será testada
 import static org.junit.jupiter.api.Assertions.assertEquals; // Asserts do JUnit 5
+
+import org.junit.jupiter.api.Test; // JUnit 5
 import org.junit.jupiter.params.ParameterizedTest; // DDT
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource; // DDT
+
+import com.iterasys.Main; // Classe que será testada
 
 // 3- Classe
 public class TestCalculadora {
@@ -62,23 +64,23 @@ public class TestCalculadora {
     // DDT - Data Driven Testing -> Teste directionado a dados
     // Popular -> Teste com massa de dados
     @ParameterizedTest
-     @CsvSource(value={
-        "15, 25, 40.0", 
-        "0, 12, 12.0",
-        "-5, 9, 4.0",
-        "-7, -8, -15.0",
-        "0.5, 3.7, 4.2"
-     }, delimiter = ',')
+    @CsvSource(value = {
+            "15, 25, 40.0",
+            "0, 12, 12.0",
+            "-5, 9, 4.0",
+            "-7, -8, -15.0",
+            "0.5, 3.7, 4.2"
+    }, delimiter = ',')
 
-     public void testSomarDDT(float num1, float num2, float resultEsperado) { 
-         // Os dados de entrada e o resultado esperado são lidos da massa de teste acima
-         float resultAtual = Main.somar(num1, num2); // Execução
-         assertEquals(resultEsperado, resultAtual); // Verificação
-     }
+    public void testSomarDDT(float num1, float num2, float resultEsperado) {
+        // Os dados de entrada e o resultado esperado são lidos da massa de teste acima
+        float resultAtual = Main.somar(num1, num2); // Execução
+        assertEquals(resultEsperado, resultAtual); // Verificação
+    }
 
-     @ParameterizedTest
-     @CsvFileSource(resources = "/csv/somar.csv", numLinesToSkip = 1, delimiter = ',')
-     public void testSomarCSV(float num1, float num2, float resultEsperado) { 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/csv/somar.csv", numLinesToSkip = 1, delimiter = ',')
+    public void testSomarCSV(float num1, float num2, float resultEsperado) {
         // Os dados de entrada e o resultado esperado são lidos da massa de teste acima
         float resultAtual = Main.somar(num1, num2); // Execução
         assertEquals(resultEsperado, resultAtual); // Verificação
@@ -86,7 +88,7 @@ public class TestCalculadora {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/csv/multiplicar.csv", numLinesToSkip = 1, delimiter = ',')
-    public void testMultiplicarCSV(float num1, float num2, float resultEsperado){
+    public void testMultiplicarCSV(float num1, float num2, float resultEsperado) {
         float resultAtual = Main.multiplicar(num1, num2);
         assertEquals(resultEsperado, resultAtual);
     }
